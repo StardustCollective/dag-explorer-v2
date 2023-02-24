f029ef368eaba640b724cd1659653f40bc5a6bb7
Joaquin <joaco.piersigilli@gmail.com>
2023-02-24 19:35:04 -0300

    fix env variable name missmatch

--- src/utils/reactGoogleAnalytics.ts
+++ src/utils/reactGoogleAnalytics.ts
@@ -1,11 +1,11 @@
-import ReactGA from 'react-ga4'
+import ReactGA from 'react-ga4';
 
-const isProduction = process.env.NODE_ENV === 'production'
+const isProduction = process.env.NODE_ENV === 'production';
 
 const initializeReactGaLib = async () => {
-  await ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID ?? '', {
+  await ReactGA.initialize(process.env.REACT_APP_GA4_MEASUREMENT_ID ?? '', {
     testMode: !isProduction,
-  })
-}
+  });
+};
 
-export { initializeReactGaLib }
+export { initializeReactGaLib };
