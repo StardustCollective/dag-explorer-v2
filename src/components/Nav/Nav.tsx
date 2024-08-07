10d006afce9a85206fd003ede2b56f68fb0a58ce
Juan David Suarez <juansuarezespitia@gmail.com>
2024-08-07 09:51:00 -0500

    Updated: navigation component => proper anchor

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,21 +1,17 @@
 import { NavDropdown } from './NavDropdown';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/icons/HeaderLogo.svg';
 import styles from './Nav.module.scss';
 
 export const Nav = () => {
-  const navigate = useNavigate();
   return (
     <>
       <nav className={styles.fullWidth}>
         <div className={styles.maxWidth}>
-          <div
-            className={styles.leftSide}
-            onClick={() => (location.pathname === '/' ? window.location.reload() : navigate('/'))}
-          >
+          <Link to={'/'} className={styles.leftSide}>
             <img className={styles.navSeparator} src={Logo} />
             <p className={styles.title}>DAG Explorer</p>
-          </div>
+          </Link>
           <div className={styles.rightSide}>
             <NavDropdown />
             <p>USD</p>
