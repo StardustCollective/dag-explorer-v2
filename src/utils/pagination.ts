25f3b440ef9aec8f5238caaf7f20f8763b711e08
Juan David Suarez <juansuarezespitia@gmail.com>
2024-08-08 09:13:01 -0500

    Added: useNextTokenPagination hook

--- src/utils/pagination.ts
+++ src/utils/pagination.ts
@@ -99,11 +99,11 @@ export const handleFetchedData = (
 };
 
 export const usePagination = (pageSize = 10, defaultPage = 0) => {
-  const [totalItems, setTotalItems] = useState(0);
+  const [totalItems, setTotalItems] = useState<number | null>(0);
   const [currentPage, setCurrentPage] = useState(defaultPage);
   const [currentPageSize, setCurrentPageSize] = useState(pageSize);
 
-  const totalPages = Math.ceil(totalItems / currentPageSize);
+  const totalPages = totalItems ? Math.ceil(totalItems / currentPageSize) : null;
 
   const actions = {
     currentPage,
@@ -118,13 +118,67 @@ export const usePagination = (pageSize = 10, defaultPage = 0) => {
       return actions.goPage(currentPage - numPages);
     },
     goPage: (page: number) => {
-      const newPage = Math.min(totalPages - 1, Math.max(0, page));
-      setCurrentPage(newPage);
-      return newPage;
+      if (totalPages) {
+        page = Math.min(totalPages - 1, page);
+      }
+
+      page = Math.max(0, page);
+
+      setCurrentPage(page);
+      return page;
     },
     setPageSize: (size: number) => {
       setCurrentPage(0);
       setCurrentPageSize(size);
+    },
+    setTotalItems,
+  };
+
+  useEffect(() => {
+    if (currentPageSize !== pageSize) {
+      actions.setPageSize(pageSize);
+    }
+  }, [pageSize]);
+
+  return actions;
+};
+
+export const useNextTokenPagination = (pageSize = 10) => {
+  const [totalItems, setTotalItems] = useState<number | null>(0);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPageSize, setCurrentPageSize] = useState(pageSize);
+  const [pageTokens, setPageTokens] = useState<Record<number, string | null>>({});
+
+  const totalPages = totalItems ? Math.ceil(totalItems / currentPageSize) : null;
+
+  const actions = {
+    currentPage,
+    currentPageSize,
+    limit: currentPageSize,
+    pageToken: pageTokens[currentPage] ?? null,
+    totalPages,
+    goNextPage: (numPages = 1) => {
+      return actions.goPage(currentPage + numPages);
+    },
+    goPreviousPage: (numPages = 1) => {
+      return actions.goPage(currentPage - numPages);
+    },
+    goPage: (page: number) => {
+      if (totalPages) {
+        page = Math.min(totalPages - 1, page);
+      }
+
+      page = Math.max(0, page);
+
+      setCurrentPage(page);
+      return page;
+    },
+    setPageSize: (size: number) => {
+      setCurrentPage(0);
+      setCurrentPageSize(size);
+    },
+    setNextPageToken: (token: string) => {
+      setPageTokens((s) => ({ ...s, [currentPage + 1]: token }));
     },
     setTotalItems,
   };
