9e033b48d04b735169e04cb5ad2c1d6cf1cab599
Juan David Suarez <juansuarezespitia@gmail.com>
2024-08-12 13:15:56 -0500

    Added: support for react elements on shorten() fn

--- src/utils/shorten.ts
+++ src/utils/shorten.ts
@@ -1,4 +1,12 @@
-export const shorten = (value: string | undefined | null, prefixLength = 5, suffixLength = 5, separator = '...') => {
+import React from 'react';
+
+export const shorten = (
+  value: string | undefined | null | React.ReactElement,
+  prefixLength = 5,
+  suffixLength = 5,
+  separator = '...'
+) => {
+  value = React.isValidElement(value) ? '' : value as string | null | undefined;
   value = value ?? '';
   return value.substring(0, prefixLength) + separator + value.substring(value.length - suffixLength);
 };
