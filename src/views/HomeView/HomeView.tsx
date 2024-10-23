0d81660b0352e6024fced2c6e63fc82ee1d823a0
Juan David Suarez <juansuarezespitia@gmail.com>
2024-10-23 11:05:34 -0500

    Added: home view metagraph projects table

--- src/views/HomeView/HomeView.tsx
+++ src/views/HomeView/HomeView.tsx
@@ -1,12 +1,18 @@
 import { useContext, useState } from 'react';
 import { SearchBar } from '../../components/SearchBar/SearchBar';
 import StatsSection from './StatsSection/StatsSection';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { NetworkContext, NetworkContextType } from '../../context/NetworkContext';
 import MainnetOneHomeTables from './MainnetOneHomeTables';
 import HomeTables from './HomeTables';
 
 import styles from './HomeView.module.scss';
+import { Subheader } from '../../components/Subheader/Subheader';
+
+import { useGetAllMetagraphProjects } from '../../api/block-explorer';
+import { ReactComponent as ConstellationGrayIcon } from '../../assets/icons/constellation-gray.svg';
+import clsx from 'clsx';
+import { Table } from '../../components/Table';
 
 const REFETCH_EVERY = 15000;
 
@@ -16,10 +22,17 @@ export const HomeView = () => {
 
   const [error, setError] = useState(false);
 
+  const metagraphs = useGetAllMetagraphProjects({
+    limit: 5,
+    offset: 0,
+  });
+
   const handleError = () => {
     setError(true);
   };
-  const LIMIT = ['mainnet', 'mainnet1'].includes(network) ? 10 : 5
+  const LIMIT = ['mainnet', 'mainnet1'].includes(network) ? 10 : 5;
+  const numberFormat = new Intl.NumberFormat('en-US', { maximumFractionDigits: 0 });
+
   return (
     <>
       <section className={`${styles.fullWidth} ${styles.section}`}>
@@ -30,6 +43,50 @@ export const HomeView = () => {
       <main className={`${styles.fullWidth2} background`}>
         <div className={`${styles.row} ${styles.fila1}`}>
           <StatsSection />
+        </div>
+        <div className={clsx(styles.row, styles.metagraphs)}>
+          <Table
+            header="Top Projects"
+            primaryKey="id"
+            titles={{
+              name: { content: 'Project' },
+              type: { content: 'Type' },
+              snapshots90d: { content: 'Snapshots (90D)' },
+              fees90d: { content: 'Fees (90D)' },
+              feesTotal: { content: 'Total Fees' },
+            }}
+            showSkeleton={!metagraphs.isFetched ? { size: 5 } : null}
+            emptyStateLabel="No metagraphs detected"
+            data={metagraphs.data?.data ?? []}
+            formatData={{
+              name: (value, record) =>
+                record.metagraphId ? (
+                  <Link to={`/metagraphs/${record.metagraphId}`} className={styles.metagraphLink}>
+                    {record.icon_url ? <img src={record.icon_url} /> : <ConstellationGrayIcon />}
+                    {value}
+                  </Link>
+                ) : (
+                  <span className={styles.metagraphLink}>
+                    {record.icon_url ? <img src={record.icon_url} /> : <ConstellationGrayIcon />}
+                    {value}
+                  </span>
+                ),
+              type: (value) => <span className={clsx(styles.metagraphType, styles[value])}>{value}</span>,
+              snapshots90d: (value) => (
+                <span className={styles.metagraphNumber}>{value === null ? 'Hidden' : numberFormat.format(value)}</span>
+              ),
+              fees90d: (value) => (
+                <span className={styles.metagraphNumber}>
+                  {value === null ? 'Hidden' : numberFormat.format(value / 1e8) + ' DAG'}
+                </span>
+              ),
+              feesTotal: (value) => (
+                <span className={styles.metagraphNumber}>
+                  {value === null ? 'Hidden' : numberFormat.format(value / 1e8) + ' DAG'}
+                </span>
+              ),
+            }}
+          />
         </div>
         <div className={`${styles.row} ${styles.fila2}`}>
           {networkVersion === '1.0' && (
@@ -45,7 +102,7 @@ export const HomeView = () => {
               </div>
             </div>
           )}
-          {networkVersion === '2.0' && <HomeTables limit={LIMIT} refetchEvery={REFETCH_EVERY} network={network}/>}
+          {networkVersion === '2.0' && <HomeTables limit={LIMIT} refetchEvery={REFETCH_EVERY} network={network} />}
         </div>
       </main>
     </>
