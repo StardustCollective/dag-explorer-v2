7b97739661ce0ecaa74241da263dc4942272eb52
Juan David Suarez <juansuarezespitia@gmail.com>
2024-10-23 10:53:12 -0500

    Updated: metagraphs table

--- src/views/MetagraphsView/view.tsx
+++ src/views/MetagraphsView/view.tsx
@@ -1,21 +1,26 @@
-import { useGetAllMetagraphs } from '../../api/block-explorer/metagraphs';
+import { useGetAllMetagraphProjects } from '../../api/block-explorer/metagraphs';
 import { Subheader } from '../../components/Subheader/Subheader';
 
 import styles from './view.module.scss';
 import { TablePagination } from '../../components/TablePagination/component';
 import { Table } from '../../components/Table';
-import { shorten } from '../../utils/shorten';
+
 import { Link, Navigate } from 'react-router-dom';
 import { usePagination } from '../../utils/pagination';
 import { useContext, useEffect } from 'react';
 import { ViewLayout } from '../../components/ViewLayout/component';
 import { NetworkContext } from '../../context/NetworkContext';
+import { ReactComponent as ConstellationGrayIcon } from '../../assets/icons/constellation-gray.svg';
+import clsx from 'clsx';
 
 export const MetagraphsView = () => {
   const { network } = useContext(NetworkContext);
 
   const metagraphsPagination = usePagination(10);
-  const metagraphs = useGetAllMetagraphs({ limit: metagraphsPagination.limit, offset: metagraphsPagination.offset });
+  const metagraphs = useGetAllMetagraphProjects({
+    limit: metagraphsPagination.limit,
+    offset: metagraphsPagination.offset,
+  });
 
   useEffect(() => {
     metagraphs.isFetched && metagraphsPagination.setTotalItems(metagraphs.data.meta.total);
@@ -25,33 +30,50 @@ export const MetagraphsView = () => {
     return <Navigate to="/" />;
   }
 
+  const numberFormat = new Intl.NumberFormat('en-US', { maximumFractionDigits: 0 });
+
   return (
     <ViewLayout className={styles.main}>
       <Subheader text="Metagraphs" />
       <div className={styles.metagraphs}>
         <Table
-          primaryKey="metagraphId"
+          primaryKey="id"
           titles={{
-            metagraphName: { content: 'Metagraph' },
-            metagraphSymbol: { content: 'Symbol' },
-            metagraphId: { content: 'Metagraph Id' },
-            metagraphSiteUrl: { content: 'Website' },
+            name: { content: 'Project' },
+            type: { content: 'Type' },
+            snapshots90d: { content: 'Snapshots (90D)' },
+            fees90d: { content: 'Fees (90D)' },
+            feesTotal: { content: 'Total Fees' },
           }}
           showSkeleton={!metagraphs.isFetched ? { size: metagraphsPagination.currentPageSize } : null}
           emptyStateLabel="No metagraphs detected"
           data={metagraphs.data?.data ?? []}
           formatData={{
-            metagraphName: (value, record) => (
-              <Link to={`/metagraphs/${record.metagraphId}`} className={styles.metagraphLink}>
-                <img src={record.metagraphIcon} />
-                {value}
-              </Link>
+            name: (value, record) =>
+              record.metagraphId ? (
+                <Link to={`/metagraphs/${record.metagraphId}`} className={styles.metagraphLink}>
+                  {record.icon_url ? <img src={record.icon_url} /> : <ConstellationGrayIcon />}
+                  {value}
+                </Link>
+              ) : (
+                <span className={styles.metagraphLink}>
+                  {record.icon_url ? <img src={record.icon_url} /> : <ConstellationGrayIcon />}
+                  {value}
+                </span>
+              ),
+            type: (value) => <span className={clsx(styles.metagraphType, styles[value])}>{value}</span>,
+            snapshots90d: (value) => (
+              <span className={styles.metagraphNumber}>{value === null ? 'Hidden' : numberFormat.format(value)}</span>
             ),
-            metagraphId: (value) => <Link to={`/metagraphs/${value}`}>{shorten(value)}</Link>,
-            metagraphSiteUrl: (value) => (
-              <a className={styles.metagraphSiteLink} target="_blank" href={value} rel="noreferrer">
-                {value}
-              </a>
+            fees90d: (value) => (
+              <span className={styles.metagraphNumber}>
+                {value === null ? 'Hidden' : numberFormat.format(value / 1e8) + ' DAG'}
+              </span>
+            ),
+            feesTotal: (value) => (
+              <span className={styles.metagraphNumber}>
+                {value === null ? 'Hidden' : numberFormat.format(value / 1e8) + ' DAG'}
+              </span>
             ),
           }}
         />
