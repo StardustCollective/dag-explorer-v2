954250c52dcbe0b3849395bdcf23d1baa528a41c
Juan David Suarez <juansuarezespitia@gmail.com>
2024-11-26 12:32:46 -0500

    Updated: export rewards modal to use new rewards API

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,15 +1,14 @@
 export const api = {
-  get: <T>(url: string, params?: object): Promise<T> => {
-    const searchUrl = Object.keys(params).length > 0 ? url + '?' + new URLSearchParams({ ...params }) : url;
-    return fetch(searchUrl)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(response.status.toString());
-        }
-        return response.json() as Promise<T>;
-      })
-      .catch((error: Error) => {
-        throw error;
-      });
+  get: async <T>(url: string, params?: Record<any, any>): Promise<T> => {
+    const searchParams = new URLSearchParams({ ...params });
+    const searchUrl = url + '?' + searchParams;
+
+    const response = await fetch(searchUrl);
+
+    if (!response.ok) {
+      throw new Error(response.status.toString());
+    }
+
+    return response.json();
   },
 };
