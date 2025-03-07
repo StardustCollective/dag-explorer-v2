4ebd886cdcd6d3215f0a4a5ec5a744ec8f1126e1
Juan David Suarez <juansuarezespitia@gmail.com>
2025-03-07 11:24:29 -0500

    Added: new dashboard page

---
+++ src/app/[network]/page.tsx
@@ -0,0 +1,338 @@
+import { HgtpNetwork } from "@/common/consts/network";
+import { getNetworkFromParams } from "@/common/network";
+import { AutoRefresh } from "@/components/AutoRefresh";
+import { PageLayout } from "@/components/PageLayout";
+import { Section } from "@/components/Section";
+
+import { StatCard } from "@/components/StatCard";
+import { Table } from "@/components/Table";
+import { ValidatorCard } from "@/components/ValidatorCard";
+import {
+  getLatestMetagraphSnapshots,
+  getLatestMetagraphTransactions,
+  getLatestSnapshots,
+  getLatestTransactions,
+  getMetagraphs,
+} from "@/queries";
+import {
+  formatCurrencyWithDecimals,
+  formatNumberWithDecimals,
+  shortenString,
+  stringFormat,
+} from "@/utils";
+import Decimal from "decimal.js";
+import Image from "next/image";
+import ConstellationGrayIcon from "@/assets/logos/constellation-gray.svg";
+import Link from "next/link";
+import clsx from "clsx";
+import dayjs from "dayjs";
+
+export const revalidate = 15;
+
+export const generateStaticParams = async () => {
+  return Object.values(HgtpNetwork).map((network) => ({
+    network,
+  }));
+};
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ network: string }>;
+}) {
+  const network = await getNetworkFromParams(params);
+
+  if (!network) {
+    throw new Error("Network not found");
+  }
+
+  const metagraphs = await getMetagraphs(network, { pagination: { limit: 5 } });
+
+  const dagSnapshots = await getLatestSnapshots(network, {
+    pagination: { limit: 5 },
+  });
+  const dagTransactions = await getLatestTransactions(network, {
+    pagination: { limit: 5 },
+  });
+
+  const metagraphSnapshots = await getLatestMetagraphSnapshots(network, {
+    pagination: { limit: 5 },
+  });
+  const metagraphTransactions = await getLatestMetagraphTransactions(network, {
+    pagination: { limit: 5 },
+  });
+
+  return (
+    <PageLayout className="flex flex-col gap-10 px-20 py-8" renderAs={"main"}>
+      <AutoRefresh interval={1000 * 15} />
+      <Section title="Explorer stats" className="flex flex-nowrap gap-6">
+        <StatCard label="Total Snapshots">
+          {formatNumberWithDecimals(Date.now() / 1000, { max: 0 })}
+        </StatCard>
+        <StatCard label="Total Dag Locked">
+          {formatCurrencyWithDecimals("DAG", 100 * 1e6)}
+        </StatCard>
+        <StatCard label="Total Snapshots Fees (90D)">
+          {formatCurrencyWithDecimals("DAG", 350_132)}
+        </StatCard>
+        <StatCard label="Total Snapshots Fees">
+          {formatCurrencyWithDecimals("DAG", 3_350_132)}
+        </StatCard>
+      </Section>
+      <Section title="Top validators" className="flex flex-nowrap gap-6">
+        <ValidatorCard
+          type="metagraph"
+          subtitle="Panasonic"
+          title={shortenString("DAG6Yxge8Tzd8DJDJeL4hMLntnhheHGR4DYSPQvf")}
+          logoUrl="https://icons-metagraph.s3.amazonaws.com/DOR/dortoken_red.svg"
+          delegatedAmount={new Decimal(1e6)}
+          commissionPercentage={5}
+          description="Block of description text will go here. It will be 2 to 3 lines maximum before we start truncating the paragraph. This should give us enough space to describe the node..."
+        />
+        <ValidatorCard
+          type="metagraph"
+          subtitle="DOR"
+          title={shortenString("DAG6Yxge8Tzd8DJDJeL4hMLntnhheHGR4DYSPQvf")}
+          logoUrl="https://icons-metagraph.s3.amazonaws.com/DOR/dortoken_red.svg"
+          delegatedAmount={new Decimal(1e6)}
+          commissionPercentage={5}
+          description="Powering the Constellation Network with secure, high-performance validation. Maximizing uptime, optimizing rewards, and decentralizing the future..."
+        />
+        <ValidatorCard
+          type="validator"
+          subtitle="Panasonic"
+          title="Joao's node"
+          logoUrl="https://icons-metagraph.s3.amazonaws.com/DOR/dortoken_red.svg"
+          delegatedAmount={new Decimal(1e5)}
+          commissionPercentage={5}
+          description="Block of description text will go here. It will be 2 to 3 lines maximum before we start truncating the paragraph. This should give us enough space to describe the node..."
+        />
+      </Section>
+      <Section title="Top projects">
+        <Table
+          className="w-full [&_td]:text-sm"
+          data={metagraphs.sort((a, b) => a.type.localeCompare(b.type))}
+          primaryKey="id"
+          titles={{
+            name: "Projects",
+            type: "Type",
+            snapshots90d: "Snapshots (90D)",
+            fees90d: "Fees (90D)",
+            feesTotal: "Total Fees",
+          }}
+          format={{
+            name: (value, record) => (
+              <Link
+                className="flex flex-row gap-3 items-center text-hgtp-blue-600 font-medium"
+                href={
+                  record.metagraphId !== null
+                    ? `/metagraph/${record.metagraphId}`
+                    : "#"
+                }
+              >
+                <div className="size-8 rounded-full">
+                  {record.icon_url && (
+                    <Image
+                      className="object-contain size-8"
+                      src={record.icon_url}
+                      alt={value}
+                      width={32}
+                      height={32}
+                    />
+                  )}
+                  {!record.icon_url && (
+                    <ConstellationGrayIcon className="size-8" />
+                  )}
+                </div>
+                {value}
+              </Link>
+            ),
+            type: (value) => (
+              <span
+                className={clsx(
+                  "flex items-center px-3 py-1 rounded-3xl w-fit text-xs font-medium",
+                  value === "public" && "bg-hgtp-blue-50",
+                  value === "private" && "bg-gray-100"
+                )}
+              >
+                {stringFormat(value, "TITLE_CASE")}
+              </span>
+            ),
+            snapshots90d: (value) => (
+              <span>
+                {value === null ? "Hidden" : formatNumberWithDecimals(value)}
+              </span>
+            ),
+            fees90d: (value) => (
+              <span>
+                {value === null
+                  ? "Hidden"
+                  : formatCurrencyWithDecimals("DAG", (value ?? 0) / 1e8)}
+              </span>
+            ),
+            feesTotal: (value) => (
+              <span>
+                {value === null
+                  ? "Hidden"
+                  : formatCurrencyWithDecimals("DAG", (value ?? 0) / 1e8)}
+              </span>
+            ),
+          }}
+        />
+      </Section>
+      <div className="flex gap-6">
+        <Section
+          title="Latest DAG snapshots"
+          className={{ wrapper: "w-1/2", children: "flex flex-col gap-5" }}
+        >
+          <Table
+            className="w-full [&_td]:text-sm"
+            data={dagSnapshots}
+            primaryKey="ordinal"
+            titles={{
+              ordinal: "Ordinal",
+              timestamp: "Timestamp",
+              blocks: "Blocks",
+            }}
+            format={{
+              ordinal: (value) => (
+                <Link
+                  className="text-hgtp-blue-600"
+                  href={`/snapshots/${value}`}
+                >
+                  {value}
+                </Link>
+              ),
+              timestamp: (value) => <span>{dayjs(value).fromNow()}</span>,
+              blocks: (value) => <span>{value.length}</span>,
+            }}
+          />
+          <Link href="/snapshots" className="button secondary md w-full">
+            View all DAG snapshots
+          </Link>
+        </Section>
+        <Section
+          title="Latest DAG transactions"
+          className={{ wrapper: "w-1/2", children: "flex flex-col gap-5" }}
+        >
+          <Table
+            className="w-full [&_td]:text-sm"
+            data={dagTransactions}
+            primaryKey="hash"
+            titles={{
+              hash: "Txn Hash",
+              timestamp: "Timestamp",
+              amount: "Amount",
+            }}
+            format={{
+              hash: (value, record) => (
+                <Link
+                  className="text-hgtp-blue-600"
+                  href={`/metagraphs/${record.metagraphId}/transactions/${value}`}
+                >
+                  {shortenString(value)}
+                </Link>
+              ),
+              timestamp: (value) => <span>{dayjs(value).fromNow()}</span>,
+              amount: (value, record) => (
+                <span>
+                  {formatCurrencyWithDecimals(
+                    record.symbol ?? "--",
+                    value / 1e8
+                  )}
+                </span>
+              ),
+            }}
+          />
+          <Link href="/transactions" className="button secondary md w-full">
+            View all DAG transactions
+          </Link>
+        </Section>
+      </div>
+      <div className="flex gap-6">
+        <Section
+          title="Latest metagraph snapshots"
+          className={{ wrapper: "w-1/2", children: "flex flex-col gap-5" }}
+        >
+          <Table
+            className="w-full [&_td]:text-sm"
+            data={metagraphSnapshots}
+            primaryKey="ordinal"
+            titles={{
+              metagraphName: "Metagraph",
+              ordinal: "Ordinal",
+              timestamp: "Timestamp",
+              blocks: "Blocks",
+            }}
+            format={{
+              metagraphName: (value, record) => (
+                <Link
+                  className="text-hgtp-blue-600"
+                  href={`/metagraphs/${record.metagraphId}`}
+                >
+                  {record.symbol}
+                </Link>
+              ),
+              ordinal: (value, record) => (
+                <Link
+                  className="text-hgtp-blue-600"
+                  href={`/metagraphs/${record.metagraphId}/snapshots/${value}`}
+                >
+                  {value}
+                </Link>
+              ),
+              timestamp: (value) => <span>{dayjs(value).fromNow()}</span>,
+              blocks: (value) => <span>{value.length}</span>,
+            }}
+          />
+          <Link
+            href="/metagraphs/snapshots"
+            className="button secondary md w-full"
+          >
+            View all metagraph snapshots
+          </Link>
+        </Section>
+        <Section
+          title="Latest metagraph transactions"
+          className={{ wrapper: "w-1/2", children: "flex flex-col gap-5" }}
+        >
+          <Table
+            className="w-full [&_td]:text-sm"
+            data={metagraphTransactions}
+            primaryKey="hash"
+            titles={{
+              hash: "Txn Hash",
+              timestamp: "Timestamp",
+              amount: "Amount",
+            }}
+            format={{
+              hash: (value, record) => (
+                <Link
+                  className="text-hgtp-blue-600"
+                  href={`/metagraphs/${record.metagraphId}/transactions/${value}`}
+                >
+                  {shortenString(value)}
+                </Link>
+              ),
+              timestamp: (value) => <span>{dayjs(value).fromNow()}</span>,
+              amount: (value, record) => (
+                <span>
+                  {formatCurrencyWithDecimals(
+                    record.symbol ?? "--",
+                    value / 1e8
+                  )}
+                </span>
+              ),
+            }}
+          />
+          <Link
+            href="/metagraphs/transactions"
+            className="button secondary md w-full"
+          >
+            View all metagraph transactions
+          </Link>
+        </Section>
+      </div>
+    </PageLayout>
+  );
+}
