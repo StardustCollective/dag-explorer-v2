63cbb6544c84e3879c06d7d3949765b7280e9bd0
Juan David Suarez <juansuarezespitia@gmail.com>
2025-03-10 17:29:08 -0500

    Added: block explorer APIs

--- src/common/apis.ts
+++ src/common/apis.ts
@@ -1,5 +1,6 @@
 import { DeploymentStage, EnvironmentContext } from "@/runtime";
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
+import { HgtpNetwork } from "./consts"; 
 
 export const DagExplorerAPI = axios.create({
   baseURL: `https://${
@@ -16,3 +17,18 @@ export const ConstellationEcosystemAPI = axios.create({
       : "staging"
   }.ecosystem-api.constellationnetwork.net`,
 });
+
+export const BlockExplorerAPI: Record<HgtpNetwork, AxiosInstance> = {
+  [HgtpNetwork.MAINNET]: axios.create({
+    baseURL: `https://be-mainnet.constellationnetwork.io`,
+  }),
+  [HgtpNetwork.INTEGRATIONNET]: axios.create({
+    baseURL: `https://be-integrationnet.constellationnetwork.io`,
+  }),
+  [HgtpNetwork.TESTNET]: axios.create({
+    baseURL: `https://be-testnet.constellationnetwork.io`,
+  }),
+  [HgtpNetwork.MAINNET_1]: axios.create({
+    baseURL: `https://block-explorer.constellationnetwork.io`,
+  }),
+};
