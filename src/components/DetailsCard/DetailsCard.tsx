31074ffb1069bb81fcda8f8d4d341a9c633697fa
Juan David Suarez <juansuarezespitia@gmail.com>
2025-03-10 17:30:32 -0500

    Added: details card

---
+++ src/components/DetailsCard/DetailsCard.tsx
@@ -0,0 +1,44 @@
+import clsx from "clsx";
+
+export type IDetailsCardProps = {
+  rows: (
+    | { label?: React.ReactNode; value?: React.ReactNode }
+    | null
+    | undefined
+  )[];
+  className?: string;
+};
+
+export const DetailsCard = ({ rows, className }: IDetailsCardProps) => {
+  return (
+    <table
+      className={clsx(
+        "card rounded-xl border-separate border-spacing-0",
+        className
+      )}
+    >
+      <tbody>
+        {rows.map(
+          (row, idx) =>
+            row && (
+              <tr
+                key={idx}
+                className={clsx(
+                  "[&_td]:py-5",
+                  "[&_td]:px-2",
+                  "[&_td:first-child]:pl-6",
+                  "[&_td:last-child]:pr-6",
+                  "[&_td]:border-b-gray-200 [&_td]:border-b"
+                )}
+              >
+                <td className="font-semibold text-hgtp-blue-950 opacity-65 w-[20%] max-w-[200px] whitespace-nowrap">
+                  {row.label}
+                </td>
+                <td className="text-gray-900">{row.value}</td>
+              </tr>
+            )
+        )}
+      </tbody>
+    </table>
+  );
+};
