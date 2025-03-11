8aa28b2864a1ec1cba8912041daee31888612459
Juan David Suarez <juansuarezespitia@gmail.com>
2025-03-11 08:27:45 -0500

    Added: wallet connection in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,18 @@
+"use client";
+
 import Link from "next/link";
 import { PageLayout } from "../PageLayout";
+import { useWalletProvider } from "@/providers/WalletProvider";
+import { useState } from "react";
+import { MenuCard } from "../MenuCard";
+import { shortenString } from "@/utils";
 
 export type IHeaderProps = Record<string, never>;
 
 export const Header = ({}: IHeaderProps) => {
+  const { wallet } = useWalletProvider();
+  const [walletDropdownOpen, setWalletDropdownOpen] = useState(false);
+
   return (
     <PageLayout
       className={{
@@ -12,23 +21,66 @@ export const Header = ({}: IHeaderProps) => {
           "flex justify-between items-center h-20 px-20 text-white font-medium",
       }}
     >
-      <Link className="flex gap-2.5 font-twk-laus font-medium text-2xl" href="/">
+      <Link
+        className="flex gap-2.5 font-twk-laus font-medium text-2xl"
+        href="/"
+      >
         DAG Explorer
       </Link>
       <div className="flex gap-4">
         <Link className="flex gap-2 items-center h-9 px-4.5" href="/metagraphs">
           Metagraphs
         </Link>
-        <Link
-          className="flex gap-2 items-center h-9 px-4.5"
-          href="/staking"
-        >
+        <Link className="flex gap-2 items-center h-9 px-4.5" href="/staking">
           Delegated staking
         </Link>
       </div>
-      <div className="flex gap-4">
-        <button className="button outline sm font-medium">Mainnet 2.0</button>
-        <button className="button primary sm">Connect wallet</button>
+      <div className="flex gap-4 relative">
+        <button className="button secondary outlined sm font-medium bg-transparent">
+          Mainnet 2.0
+        </button>
+        {!wallet.active && (
+          <button
+            className="button primary sm"
+            onClick={() => wallet.activate()}
+          >
+            Connect wallet
+          </button>
+        )}
+        {wallet.active && (
+          <button
+            className="button secondary outlined sm font-medium bg-transparent"
+            onClick={() => setWalletDropdownOpen((s) => !s)}
+          >
+            {shortenString(wallet.account)}
+          </button>
+        )}
+        {wallet.active && walletDropdownOpen && (
+          <MenuCard
+            className="absolute top-full right-0 mt-2.5 w-[244px] z-10"
+            afterContent={
+              <button
+                className="button primary outlined sm font-medium bg-transparent"
+                onClick={() => wallet.deactivate()}
+              >
+                Disconnect wallet
+              </button>
+            }
+          >
+            <MenuCard.Option
+              renderAs={Link}
+              href={`/address/${wallet.account}`}
+            >
+              Address details
+            </MenuCard.Option>
+            <MenuCard.Option
+              renderAs={Link}
+              href={`/address/${wallet.account}/delegations`}
+            >
+              My delegations
+            </MenuCard.Option>
+          </MenuCard>
+        )}
       </div>
     </PageLayout>
   );
