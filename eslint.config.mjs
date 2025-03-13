bc61278b2c429c31232de5e2e8b5e6e91864d2fb
Juan David Suarez <juansuarezespitia@gmail.com>
2025-03-13 12:30:44 -0500

    Added: import eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import EslintImportPlugin from "eslint-plugin-import";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,11 +13,43 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    settings: {},
+    plugins: {
+      import: EslintImportPlugin,
+    },
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "warn",
       "@typescript-eslint/no-unused-expressions": "off",
-      "react-hooks/rules-of-hooks": "warn"
+      "react-hooks/rules-of-hooks": "warn",
+      ...EslintImportPlugin.configs.recommended.rules,
+      "import/no-unresolved": [
+        2,
+        {
+          ignore: [
+            ".svg$",
+            ".png$",
+            ".jpg$",
+            ".jpeg$",
+            ".gif$",
+            ".webp$",
+            ".ico$",
+          ],
+        },
+      ],
+      "import/order": [
+        "error",
+        {
+          "newlines-between": "always",
+          pathGroups: [
+            {
+              pattern: "@/**",
+              group: "internal",
+              position: "after",
+            },
+          ],
+        },
+      ],
     },
   },
 ];
