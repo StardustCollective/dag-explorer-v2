a95430af60343895726cd6ec307b877c1f337ed8
Juan David Suarez <juansuarezespitia@gmail.com>
2025-03-20 10:23:53 -0500

    Added: tooltip support for statcard

--- src/components/StatCard/StatCard.tsx
+++ src/components/StatCard/StatCard.tsx
@@ -1,5 +1,7 @@
 import clsx from "clsx";
 
+import { InfoTooltip } from "../Tooltip";
+
 export type IStatCardProps = {
   label?: React.ReactNode;
   tooltip?: React.ReactNode;
@@ -7,11 +9,22 @@ export type IStatCardProps = {
   children?: React.ReactNode;
 };
 
-export const StatCard = ({ label, className, children }: IStatCardProps) => {
+export const StatCard = ({
+  label,
+  tooltip,
+  className,
+  children,
+}: IStatCardProps) => {
   return (
-    <div className={clsx("card flex flex-col gap-3 p-5 w-full border-0.5", className)}>
-      <span className="text-xs text-gray-500 font-semibold uppercase">
+    <div
+      className={clsx(
+        "card flex flex-col gap-3 p-5 w-full border-0.5",
+        className
+      )}
+    >
+      <span className="flex items-center text-xs text-gray-500 font-semibold uppercase">
         {label}
+        {tooltip && <InfoTooltip  content={tooltip} className="size-4" />}
       </span>
       <span className="text-cd14 font-medium text-2xl">{children}</span>
     </div>
