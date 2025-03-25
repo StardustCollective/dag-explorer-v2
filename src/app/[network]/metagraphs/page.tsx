451830854a777bd42dabadf82553d8fdb71da2c8
Juan David Suarez <juansuarezespitia@gmail.com>
2025-03-25 15:41:07 -0500

    Added: preliminary metagraphs list page

--- src/app/[network]/metagraphs/page.tsx
+++ src/app/[network]/metagraphs/page.tsx
@@ -5,7 +5,9 @@ import Link from "next/link";
 import { HgtpNetwork } from "@/common/consts/network";
 import { getNetworkFromParamsOrFail } from "@/common/network";
 import { PageLayout } from "@/components/PageLayout";
+import { PageTitle } from "@/components/PageTitle";
 import { Section } from "@/components/Section";
+import { SkeletonSpan } from "@/components/SkeletonSpan";
 import { Table } from "@/components/Table";
 import {
   getMetagraphs,
@@ -16,7 +18,9 @@ import {
   stringFormat,
 } from "@/utils";
 
+import Brain2Icon from "@/assets/icons/brain-2.svg";
 import ConstellationCircleGrayIcon from "@/assets/logos/constellation-circle-gray.svg";
+
 
 export const revalidate = 15;
 
@@ -35,82 +39,96 @@ export default async function MetagraphsPage({
 }) {
   const network = await getNetworkFromParamsOrFail(params);
 
-  const metagraphs = await getMetagraphs(network, { pagination: { limit: 5 } });
+  const metagraphs = getMetagraphs(network, { pagination: { limit: 10 } });
 
   return (
-    <PageLayout className="flex flex-col gap-10 px-20 py-8" renderAs={"main"}>
-      <Section title="Metagraphs">
-        <Table
-          className="w-full [&_td]:text-sm"
-          data={metagraphs.sort((a, b) => a.type.localeCompare(b.type))}
-          primaryKey="id"
-          titles={{
-            name: "Projects",
-            type: "Type",
-            snapshots90d: "Snapshots (90D)",
-            fees90d: "Fees (90D)",
-            feesTotal: "Total Fees",
-          }}
-          format={{
-            name: (value, record) => (
-              <Link
-                className="flex flex-row gap-3 items-center text-hgtp-blue-600 font-medium"
-                href={
-                  record.metagraphId !== null
-                    ? `/metagraph/${record.metagraphId}`
-                    : "#"
-                }
-              >
-                <div className="size-8 rounded-full">
-                  {record.icon_url && (
-                    <Image
-                      className="object-contain size-8"
-                      src={record.icon_url}
-                      alt={value}
-                      width={32}
-                      height={32}
-                    />
+    <>
+    <PageTitle icon={<Brain2Icon className="size-8" />}>
+        Metagraphs
+      </PageTitle>
+      <PageLayout className="flex flex-col gap-10 px-20 py-8" renderAs={"main"}>
+        <Section title="Top projects">
+          <Table.Suspense
+            className="w-full [&_td]:text-sm"
+            data={metagraphs.then((data) =>
+              data.sort((a, b) => a.type.localeCompare(b.type))
+            )}
+            primaryKey="id"
+            titles={{
+              name: "Projects",
+              type: "Type",
+              snapshots90d: "Snapshots (90D)",
+              fees90d: "Fees (90D)",
+              feesTotal: "Total Fees",
+            }}
+            loadingData={SkeletonSpan.generateTableRecords(5, [
+              "name",
+              "type",
+              "snapshots90d",
+              "fees90d",
+              "feesTotal",
+            ])}
+            format={{
+              name: (value, record) => (
+                <Link
+                  className="flex flex-row gap-3 items-center text-hgtp-blue-600 font-medium"
+                  href={
+                    record.metagraphId !== null
+                      ? `/metagraph/${record.metagraphId}`
+                      : "#"
+                  }
+                >
+                  <div className="size-8 rounded-full">
+                    {record.icon_url && (
+                      <Image
+                        className="object-contain size-8"
+                        src={record.icon_url}
+                        alt={value}
+                        width={32}
+                        height={32}
+                      />
+                    )}
+                    {!record.icon_url && (
+                      <ConstellationCircleGrayIcon className="size-8" />
+                    )}
+                  </div>
+                  {value}
+                </Link>
+              ),
+              type: (value) => (
+                <span
+                  className={clsx(
+                    "flex items-center px-3 py-1 rounded-3xl w-fit text-xs font-medium",
+                    value === "public" && "bg-hgtp-blue-50",
+                    value === "private" && "bg-gray-100"
                   )}
-                  {!record.icon_url && (
-                    <ConstellationCircleGrayIcon className="size-8" />
-                  )}
-                </div>
-                {value}
-              </Link>
-            ),
-            type: (value) => (
-              <span
-                className={clsx(
-                  "flex items-center px-3 py-1 rounded-3xl w-fit text-xs font-medium",
-                  value === "public" && "bg-hgtp-blue-50",
-                  value === "private" && "bg-gray-100"
-                )}
-              >
-                {stringFormat(value, "TITLE_CASE")}
-              </span>
-            ),
-            snapshots90d: (value) => (
-              <span>
-                {value === null ? "Hidden" : formatNumberWithDecimals(value)}
-              </span>
-            ),
-            fees90d: (value) => (
-              <span>
-                {value === null
-                  ? "Hidden"
-                  : formatCurrencyWithDecimals("DAG", (value ?? 0) / 1e8)}
-              </span>
-            ),
-            feesTotal: (value) => (
-              <span>
-                {value === null
-                  ? "Hidden"
-                  : formatCurrencyWithDecimals("DAG", (value ?? 0) / 1e8)}
-              </span>
-            ),
-          }}
-        />
-      </Section>
-    </PageLayout>
+                >
+                  {stringFormat(value, "TITLE_CASE")}
+                </span>
+              ),
+              snapshots90d: (value) => (
+                <span>
+                  {value === null ? "Hidden" : formatNumberWithDecimals(value)}
+                </span>
+              ),
+              fees90d: (value) => (
+                <span>
+                  {value === null
+                    ? "Hidden"
+                    : formatCurrencyWithDecimals("DAG", (value ?? 0) / 1e8)}
+                </span>
+              ),
+              feesTotal: (value) => (
+                <span>
+                  {value === null
+                    ? "Hidden"
+                    : formatCurrencyWithDecimals("DAG", (value ?? 0) / 1e8)}
+                </span>
+              ),
+            }}
+          />
+        </Section>
+      </PageLayout>
+    </>
   );
 }
