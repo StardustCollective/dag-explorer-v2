4821d80fa76215e4fdc339f57615dcc7d2a2ad73
Juan David Suarez <juansuarezespitia@gmail.com>
2025-03-28 13:51:21 -0500

    Added: support for promise based values on format currency component

--- src/components/FormatCurrency/FormatCurrency.tsx
+++ src/components/FormatCurrency/FormatCurrency.tsx
@@ -3,23 +3,33 @@ import React from "react";
 import { SkeletonSpan } from "../SkeletonSpan";
 import { SuspenseValue } from "../SuspenseValue";
 
-import { formatNumberWithDecimals } from "@/utils";
+import { formatNumberWithDecimals, isPromiseLike } from "@/utils";
 
 export type IFormatCurrencyProps = {
-  value: IDecimal;
+  value: Promise<IDecimal> | IDecimal;
   currency: Promise<React.ReactNode> | React.ReactNode;
   decimals?: { min?: number; max?: number };
+  className?: string;
 };
 
 export const FormatCurrency = ({
   value,
   currency,
   decimals,
+  className,
 }: IFormatCurrencyProps) => {
   return (
-    <span>
-      {formatNumberWithDecimals(value, decimals)}{" "}
-      <SuspenseValue fallback={<SkeletonSpan />} value={currency} />
-    </span>
+    <SuspenseValue
+      className={className}
+      fallback={""}
+      value={(isPromiseLike(value) ? value : Promise.resolve(value)).then(
+        (value) => (
+          <>
+            {formatNumberWithDecimals(value, decimals)}{" "}
+            <SuspenseValue fallback={<SkeletonSpan />} value={currency} />
+          </>
+        )
+      )}
+    />
   );
 };
