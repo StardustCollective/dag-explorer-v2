db95ff56ef10c8a40160452c3cfd7d03bf38ab5b
Juan David Suarez <juansuarezespitia@gmail.com>
2025-03-31 13:52:54 -0500

    Added: page title support for extra content

--- src/components/PageTitle/PageTitle.tsx
+++ src/components/PageTitle/PageTitle.tsx
@@ -4,19 +4,28 @@ import { PageLayout } from "../PageLayout";
 
 export type IPageTitleProps = {
   icon?: React.ReactNode;
+  rightContent?: React.ReactNode;
   className?: string;
   children?: React.ReactNode;
 };
 
-export const PageTitle = ({ icon, className, children }: IPageTitleProps) => {
+export const PageTitle = ({
+  icon,
+  rightContent,
+  className,
+  children,
+}: IPageTitleProps) => {
   return (
     <PageLayout
       className={{
         wrapper: "border-b border-gray-300",
-        children: clsx("flex flex-nowrap gap-2 px-20 py-5", className),
+        children: clsx("flex flex-nowrap justify-between px-20 py-5", className),
       }}
     >
-      <h1 className="flex items-center gap-2 text-hgtp-blue-900 font-medium text-2.5xl">{icon} {children}</h1>
+      <h1 className="flex items-center gap-2 text-hgtp-blue-900 font-medium text-2.5xl">
+        {icon} {children}
+      </h1>
+      {rightContent && <div>{rightContent}</div>}
     </PageLayout>
   );
 };
