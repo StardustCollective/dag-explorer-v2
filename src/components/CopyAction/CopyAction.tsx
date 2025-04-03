c303ae1d12ca53b2d2267d3e8a128e5eca75a8d7
Juan David Suarez <juansuarezespitia@gmail.com>
2025-04-03 11:18:58 -0500

    Added: toast message on copy

--- src/components/CopyAction/CopyAction.tsx
+++ src/components/CopyAction/CopyAction.tsx
@@ -4,6 +4,8 @@
 import clsx from "clsx";
 import React from "react";
 
+import { doToast } from "../Toast";
+
 import SquareBehindSquareIcon from "@/assets/icons/square-behind-square-6.svg";
 
 export type ICopyAction = {
@@ -15,6 +17,7 @@ export const CopyAction = ({ value, className }: ICopyAction) => {
   const doCopy = async () => {
     try {
       await navigator.clipboard.writeText(String(value));
+      doToast("Copied to clipboard :]");
     } catch (e) {
       console.log("Copy action failed:");
       console.error(e);
