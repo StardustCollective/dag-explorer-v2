b371fd4b37222649209e47c8f47ec1fd63d3bb28
Juan David Suarez <juansuarezespitia@gmail.com>
2025-04-25 10:03:57 -0500

    Added: page search params defaults utility

--- src/utils/search_params.ts
+++ src/utils/search_params.ts
@@ -1,3 +1,5 @@
+export type IPageSearchParams = Record<string, string | string[] | undefined>;
+
 export const buildSearchParams = (
   defaultParams: ConstructorParameters<typeof URLSearchParams>[0]
 ) => {
@@ -16,4 +18,21 @@ export const buildSearchParams = (
 
     return searchParams.toString();
   };
+};
+
+export const getPageSearchParamsOrDefaults = async <
+  SP extends IPageSearchParams,
+  DF extends IPageSearchParams
+>(
+  searchParams: SP | Promise<SP>,
+  defaultParams: DF | Promise<DF>
+) => {
+  const _searchParams = await searchParams;
+  const _defaultParams = await defaultParams;
+
+  const mergedParams = Object.assign({}, _searchParams, _defaultParams);
+
+  const nextSearchParams = buildSearchParams(mergedParams as any);
+
+  return [mergedParams, nextSearchParams] as const;
 };
