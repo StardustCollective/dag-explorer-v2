c4027c5d9a2d34e83f96bbcce3a836d9ed59a0d3
Juan David Suarez <juansuarezespitia@gmail.com>
2025-04-25 10:05:24 -0500

    Updated: staking information for node-params endpoint

--- src/types/staking.ts
+++ src/types/staking.ts
@@ -1,3 +1,11 @@
+export type IAPIMetagraphStakingNode = {
+  nodeId: string;
+  metagraphId: string;
+  name: string;
+  symbol: string;
+  iconUrl?: string | null;
+};
+
 export type IL0StakingNode = {
   id: string;
   ip: string;
@@ -5,12 +13,13 @@ export type IL0StakingNode = {
   p2pPort: number;
   clusterSession: string;
   session: string;
-  state: "Ready";
+  state: string;
   jar: string;
 };
 
 export type IL0StakingDelegator = {
-  node: IL0StakingNode;
+  peerId: string;
+  node: IL0StakingNode | null;
   delegatedStakeRewardParameters: {
     rewardFraction: number;
   };
@@ -31,10 +40,17 @@ export type IL0StakingDelegation = {
   hash: string;
   withdrawalStartEpoch: number | null;
   withdrawalEndEpoch: number | null;
+  rewardAmount: number;
+  totalBalance: number;
 };
 
 export type IL0StakingAddress = {
   address: string;
   activeDelegatedStakes: IL0StakingDelegation[];
   pendingWithdrawals: IL0StakingDelegation[];
+};
+
+export type IAPIStakingDelegator = IL0StakingDelegator & {
+  nodeIdAddress: string;
+  metagraphNode?: IAPIMetagraphStakingNode;
 };
