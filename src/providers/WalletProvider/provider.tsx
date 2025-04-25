723c9d98f37fdafd97a3b80a0a8ab4c2b7224601
Juan David Suarez <juansuarezespitia@gmail.com>
2025-04-25 09:22:28 -0500

    Updated: wallet provider to use a zustand based store

--- src/providers/WalletProvider/provider.tsx
+++ src/providers/WalletProvider/provider.tsx
@@ -1,69 +1,12 @@
 "use client";
 
-import { useStargazerWallet } from "@stardust-collective/web3-react-stargazer-connector";
-import { createContext, useContext } from "react";
+import { createWalletStore } from "./state";
 
-import { getSignableDataPayload } from "./utils";
+import { buildZustandProviderAndHook } from "@/utils/zustand_provider";
 
-type IWalletProviderContext = {
-  wallet: ReturnType<typeof useStargazerWallet>;
-  requestDataSignature: (data: Record<string, any>) => Promise<{
-    pub: string;
-    pubExp: string;
-    signature: string;
-    payload: string;
-  }>;
-};
-
-const WalletProviderContext = createContext<IWalletProviderContext | null>(
-  null
+const [WalletProvider, useWalletStore] = buildZustandProviderAndHook(
+  "Wallet",
+  createWalletStore
 );
 
-export const WalletProvider = ({
-  children,
-}: {
-  children?: React.ReactNode;
-}) => {
-  const wallet = useStargazerWallet();
-
-  const requestDataSignature: IWalletProviderContext["requestDataSignature"] =
-    async (data) => {
-      if (!wallet.active) {
-        throw new Error("Wallet is not active, cannot sign messages");
-      }
-
-      const payload = getSignableDataPayload(data);
-
-      const signature = await wallet.request({
-        method: "dag_signData",
-        params: [wallet.account, payload],
-      });
-
-      let pub: string = await wallet.request({
-        method: "dag_getPublicKey",
-        params: [wallet.account],
-      });
-
-      pub = pub.length === 128 ? pub : pub.substring(2);
-
-      return { pub, pubExp: "04" + pub, signature, payload };
-    };
-
-  return (
-    <WalletProviderContext.Provider value={{ wallet, requestDataSignature }}>
-      {children}
-    </WalletProviderContext.Provider>
-  );
-};
-
-export const useWalletProvider = () => {
-  const ctx = useContext(WalletProviderContext);
-
-  if (!ctx) {
-    throw new Error(
-      "useWalletProvider calls must be done under a <WalletProvider/> component"
-    );
-  }
-
-  return ctx;
-};
+export { WalletProvider, useWalletStore };
