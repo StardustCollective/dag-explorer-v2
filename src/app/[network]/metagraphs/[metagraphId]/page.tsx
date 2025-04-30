f0bd514888b18153bf902dfcd1de3b05b422d914
Juan David Suarez <juansuarezespitia@gmail.com>
2025-04-30 14:48:47 -0500

    Updated: seal metagraph detail page

--- src/app/[network]/metagraphs/[metagraphId]/page.tsx
+++ src/app/[network]/metagraphs/[metagraphId]/page.tsx
@@ -1,32 +1,30 @@
 import { dag4 } from "@stardust-collective/dag4";
-import dayjs from "dayjs";
-import Decimal from "decimal.js";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 
 import { NodeLayerInfo } from "./components/NodeLayerInfo";
+import { SnapshotsTable } from "./components/SnapshotsTable";
+import { TransactionsTable } from "./components/TransactionsTable";
 
-import { datumToDag } from "@/common/currencies";
 import { getNetworkFromParamsOrFail } from "@/common/network";
 import { CopyAction } from "@/components/CopyAction";
-import { FormatCurrency } from "@/components/FormatCurrency";
 import { MetagraphIcon } from "@/components/MetagraphIcon";
 import { PageLayout } from "@/components/PageLayout";
 import { PropertiesCard } from "@/components/PropertiesCard";
 import { Section } from "@/components/Section";
-import { SkeletonSpan } from "@/components/SkeletonSpan";
-import { Table } from "@/components/Table";
 import { Tab, Tabs } from "@/components/Tabs";
+import { getMetagraph, getMetagraphNodes } from "@/queries";
+import { INextTokenPaginationSearchParams } from "@/types";
 import {
-  getMetagraph,
-  getMetagraphCurrencySymbol,
-  getMetagraphNodes,
-  getSnapshots,
-  getTransactions,
-} from "@/queries";
-import { decodeDecimal, shortenString } from "@/utils";
+  
+  getPageSearchParamsOrDefaults,
+  
+  parseNumberOrDefault,
+  
+  shortenString,
+} from "@/utils";
 
-import Brain2Icon from "@/assets/icons/brain-2.svg";
+import Brain2OutlineIcon from "@/assets/icons/brain-2-outline.svg";
 import WalletIcon from "@/assets/icons/wallet.svg";
 
 export const revalidate = 30;
@@ -36,10 +34,15 @@ export default async function MetagraphPage({
   searchParams,
 }: {
   params: Promise<{ network: string; metagraphId: string }>;
-  searchParams: Promise<{ section?: string }>;
+  searchParams: Promise<{ section?: string } & INextTokenPaginationSearchParams>;
 }) {
   const { metagraphId } = await params;
-  const { section = "snapshots" } = await searchParams;
+
+  const [{section, limit}, nextSearchParams] = await getPageSearchParamsOrDefaults(searchParams, {
+    section: "snapshots",
+    limit: "10",
+  });
+
 
   const network = await getNetworkFromParamsOrFail(params);
 
@@ -54,22 +57,18 @@ export default async function MetagraphPage({
     throw notFound();
   }
 
-  const snapshots = getSnapshots(network, metagraphId, {
-    tokenPagination: { limit: 10 },
-  });
-
-  const transactions = getTransactions(network, metagraphId, {
-    tokenPagination: { limit: 10 },
-  });
 
   return (
-    <PageLayout className="flex flex-col gap-4 px-20 py-8" renderAs={"main"}>
+    <PageLayout
+      className="flex flex-col gap-4 px-4 lg:px-20 py-8"
+      renderAs={"main"}
+    >
       <Section className="flex flex-col gap-4">
         <div className="flex flex-col gap-6">
-          <div className="grid grid-cols-3 gap-4">
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
             <div className="col-span-2">
               <div className="flex flex-col gap-2">
-                <h3 className="flex items-center gap-3 text-hgtp-blue-950 font-semibold text-3xl">
+                <h3 className="flex items-center gap-3 text-hgtp-blue-950 font-semibold text-3xl justify-center lg:justify-start">
                   <MetagraphIcon
                     network={network}
                     metagraphId={metagraphId}
@@ -77,7 +76,7 @@ export default async function MetagraphPage({
                   />{" "}
                   {metagraph.name}
                 </h3>
-                <p className="max-w-[700px]">{metagraph.description}</p>
+                <p className="max-w-[700px] text-center lg:text-left">{metagraph.description}</p>
               </div>
             </div>
             <PropertiesCard
@@ -85,34 +84,39 @@ export default async function MetagraphPage({
                 {
                   label: (
                     <span className="flex items-center gap-2 text-hgtp-blue-950">
-                      <Brain2Icon className="text-black/65 size-5" /> Metagraph
-                      ID
+                      <Brain2OutlineIcon className="text-black/65 size-5 shrink-0" />{" "}
+                      Metagraph ID
                     </span>
                   ),
                   value: (
-                    <Link
-                      href={`/address/${metagraph.id}`}
-                      className="text-hgtp-blue-600"
-                    >
-                      {shortenString(metagraph.id, 4, 4)}
+                    <span className="flex items-center gap-1">
+                      <Link
+                        href={`/address/${metagraph.id}`}
+                        className="text-hgtp-blue-600"
+                      >
+                        {shortenString(metagraph.id, 4, 4)}
+                      </Link>
                       <CopyAction value={metagraph.id} />
-                    </Link>
+                    </span>
                   ),
                 },
                 {
                   label: (
                     <span className="flex items-center gap-2 text-hgtp-blue-950">
-                      <WalletIcon className="text-black/65 size-5" /> Staking
+                      <WalletIcon className="text-black/65 size-5 shrink-0" />{" "}
+                      Staking
                     </span>
                   ),
                   value: metagraph.stakingWalletAddress ? (
-                    <Link
-                      href={`/address/${metagraph.stakingWalletAddress}`}
-                      className="text-hgtp-blue-600"
-                    >
-                      {shortenString(metagraph.stakingWalletAddress, 4, 4)}
+                    <span className="flex items-center gap-1">
+                      <Link
+                        href={`/address/${metagraph.stakingWalletAddress}`}
+                        className="text-hgtp-blue-600"
+                      >
+                        {shortenString(metagraph.stakingWalletAddress, 4, 4)}
+                      </Link>
                       <CopyAction value={metagraph.stakingWalletAddress} />
-                    </Link>
+                    </span>
                   ) : (
                     ""
                   ),
@@ -120,18 +124,20 @@ export default async function MetagraphPage({
                 {
                   label: (
                     <span className="flex items-center gap-2 text-hgtp-blue-950">
-                      <WalletIcon className="text-black/65 size-5" /> Snapshot
-                      fees
+                      <WalletIcon className="text-black/65 size-5 shrink-0" />{" "}
+                      Snapshot fees
                     </span>
                   ),
                   value: metagraph.feesWalletAddress ? (
-                    <Link
-                      href={`/address/${metagraph.feesWalletAddress}`}
-                      className="text-hgtp-blue-600"
-                    >
-                      {shortenString(metagraph.feesWalletAddress, 4, 4)}
+                    <span className="flex items-center gap-1">
+                      <Link
+                        href={`/address/${metagraph.feesWalletAddress}`}
+                        className="text-hgtp-blue-600"
+                      >
+                        {shortenString(metagraph.feesWalletAddress, 4, 4)}
+                      </Link>
                       <CopyAction value={metagraph.feesWalletAddress} />
-                    </Link>
+                    </span>
                   ) : (
                     ""
                   ),
@@ -140,7 +146,7 @@ export default async function MetagraphPage({
             />
           </div>
           <div className="border-t border-black/25"></div>
-          <div className="grid grid-cols-3 gap-4">
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
             <NodeLayerInfo
               type="l0"
               layerInfo={nodes.then((nodes) => nodes?.l0)}
@@ -157,151 +163,41 @@ export default async function MetagraphPage({
         </div>
         <div className="card flex flex-col w-full">
           <Tabs value={section ?? "transactions"}>
-            <Tab id="snapshots" href={`/metagraphs/${metagraphId}`}>
+            <Tab
+              id="snapshots"
+              renderAs={Link}
+              href={`/metagraphs/${metagraphId}?${nextSearchParams({
+                section: "",
+              })}`}
+            >
               Snapshots
             </Tab>
             <Tab
               id="transactions"
-              href={`/metagraphs/${metagraphId}?section=transactions`}
+              renderAs={Link}
+              href={`/metagraphs/${metagraphId}?${nextSearchParams({
+                section: "transactions",
+              })}`}
             >
               Transactions
             </Tab>
           </Tabs>
-          {section === "snapshots" && (
-            <Table.Suspense
-              noCardStyle
-              className="w-full [&_td]:text-sm"
-              header={<span></span>}
-              data={snapshots}
-              primaryKey="ordinal"
-              titles={{
-                ordinal: "Ordinal",
-                timestamp: "Timestamp",
-                rewards: "Rewards",
-                sizeInKB: "Snapshot Size",
-                fee: "Snapshot Fee",
-              }}
-              loadingData={SkeletonSpan.generateTableRecords(5, [
-                "ordinal",
-                "timestamp",
-                "rewards",
-                "sizeInKB",
-                "fee",
-              ])}
-              format={{
-                ordinal: (value) => (
-                  <Link
-                    className="text-hgtp-blue-600"
-                    href={`/metagraphs/${metagraphId}/snapshots/${value}`}
-                  >
-                    {value}
-                  </Link>
-                ),
-                timestamp: (value) => <span>{dayjs(value).fromNow()}</span>,
-                rewards: (value) => (
-                  <FormatCurrency
-                    currency="DAG"
-                    value={datumToDag(
-                      value.reduce((acc, curr) => acc + curr.amount, 0)
-                    )}
-                  />
-                ),
-                sizeInKB: (value) => <span>{value}kb</span>,
-                fee: (value) => (
-                  <FormatCurrency currency="dDAG" value={value ?? 0} />
-                ),
-              }}
-            />
-          )}
-          {section === "transactions" && (
-            <Table.Suspense
-              noCardStyle
-              className="w-full [&_td]:text-sm"
-              header={<span></span>}
-              data={transactions}
-              primaryKey="hash"
-              titles={{
-                hash: "Txn Hash",
-                timestamp: "Timestamp",
-                snapshotOrdinal: "Snapshot",
-                fee: "Fee",
-                source: "From",
-                destination: "To",
-                amount: "Amount",
-              }}
-              loadingData={SkeletonSpan.generateTableRecords(3, [
-                "hash",
-                "timestamp",
-                "snapshotOrdinal",
-                "fee",
-                "source",
-                "destination",
-                "amount",
-              ])}
-              format={{
-                hash: (value, record) => (
-                  <Link
-                    className="text-hgtp-blue-600"
-                    href={
-                      record.metagraphId
-                        ? `/metagraphs/${record.metagraphId}/transactions/${value}`
-                        : `/transactions/${value}`
-                    }
-                  >
-                    {shortenString(value)} <CopyAction value={value} />
-                  </Link>
-                ),
-                timestamp: (value) => <span>{dayjs(value).fromNow()}</span>,
-                snapshotOrdinal: (value, record) => (
-                  <Link
-                    className="text-hgtp-blue-600"
-                    href={
-                      record.metagraphId
-                        ? `/metagraphs/${record.metagraphId}/snapshots/${value}`
-                        : `/snapshots/${value}`
-                    }
-                  >
-                    {value}
-                  </Link>
-                ),
-                fee: (value, record) => (
-                  <FormatCurrency
-                    value={value}
-                    currency={getMetagraphCurrencySymbol(
-                      network,
-                      record.metagraphId,
-                      true
-                    )}
-                  />
-                ),
-                source: (value) => (
-                  <Link
-                    className="text-hgtp-blue-600"
-                    href={`/address/${value}`}
-                  >
-                    {shortenString(value)} <CopyAction value={value} />
-                  </Link>
-                ),
-                destination: (value) => (
-                  <Link
-                    className="text-hgtp-blue-600"
-                    href={`/address/${value}`}
-                  >
-                    {shortenString(value)} <CopyAction value={value} />
-                  </Link>
-                ),
-                amount: (value, record) => (
-                  <FormatCurrency
-                    value={decodeDecimal(value).div(Decimal.pow(10, 8))}
-                    currency={getMetagraphCurrencySymbol(
-                      network,
-                      record.metagraphId
-                    )}
-                  />
-                ),
-              }}
-            />
-          )}
+          <div className="flex lg:p-0 px-4 pt-4">
+            {section === "snapshots" && (
+              <SnapshotsTable
+                network={network}
+                metagraphId={metagraphId}
+                limit={parseNumberOrDefault(limit, 10)}
+              />
+            )}
+            {section === "transactions" && (
+              <TransactionsTable
+                network={network}
+                metagraphId={metagraphId}
+                limit={parseNumberOrDefault(limit, 10)}
+              />
+            )}
+          </div>
         </div>
       </Section>
     </PageLayout>
