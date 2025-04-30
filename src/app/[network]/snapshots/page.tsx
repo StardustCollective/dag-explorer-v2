c2c3996e17277ec671fcc185469facafa21657b4
Juan David Suarez <juansuarezespitia@gmail.com>
2025-04-30 14:50:24 -0500

    Updated: seal snapshots page

--- src/app/[network]/snapshots/page.tsx
+++ src/app/[network]/snapshots/page.tsx
@@ -4,12 +4,17 @@ import Link from "next/link";
 import { HgtpNetwork } from "@/common/consts/network";
 import { datumToDag } from "@/common/currencies";
 import { getNetworkFromParamsOrFail } from "@/common/network";
+import { EmptyState } from "@/components/EmptyState";
 import { FormatCurrency } from "@/components/FormatCurrency";
 import { PageLayout } from "@/components/PageLayout";
 import { Section } from "@/components/Section";
 import { SkeletonSpan } from "@/components/SkeletonSpan";
 import { Table } from "@/components/Table";
 import { getSnapshots } from "@/queries";
+import {
+  INextTokenPaginationSearchParams,
+} from "@/types";
+import { getPageSearchParamsOrDefaults, parseNumberOrDefault } from "@/utils";
 
 export const revalidate = 15;
 
@@ -23,22 +28,31 @@ export const generateStaticParams = async () => {
 
 export default async function SnapshotsPage({
   params,
+  searchParams,
 }: {
   params: Promise<{ network: string }>;
+  searchParams: Promise<INextTokenPaginationSearchParams>;
 }) {
   const network = await getNetworkFromParamsOrFail(params);
 
+  const [{ limit }] = await getPageSearchParamsOrDefaults(searchParams, {
+    limit: "10",
+  });
+
   const snapshots = getSnapshots(network, undefined, {
-    tokenPagination: { limit: 10 },
+    tokenPagination: { limit: parseNumberOrDefault(limit, 10) },
   });
 
   return (
     <>
-      <PageLayout className="flex flex-col gap-10 px-20 py-8" renderAs={"main"}>
+      <PageLayout
+        className="flex flex-col gap-10 px-4 lg:px-20 py-8"
+        renderAs={"main"}
+      >
         <Section title="Snapshots">
           <Table.Suspense
             className="w-full [&_td]:text-sm"
-            data={snapshots}
+            data={snapshots.then((data) => data.records)}
             primaryKey="ordinal"
             titles={{
               ordinal: "Ordinal",
@@ -52,6 +66,11 @@ export default async function SnapshotsPage({
               "blocks",
               "rewards",
             ])}
+            // pagination={buildPaginationForAPIResponseArray(
+            //   snapshots,
+            //   tokenPagination.limit ?? 10
+            // )}
+            emptyState={<EmptyState label="No snapshots detected" />}
             format={{
               ordinal: (value) => (
                 <Link
@@ -67,7 +86,7 @@ export default async function SnapshotsPage({
                 <FormatCurrency
                   currency="DAG"
                   value={datumToDag(
-                    value.reduce((acc, curr) => acc + curr.amount, 0)
+                    value?.reduce((acc, curr) => acc + curr.amount, 0) ?? 0
                   )}
                 />
               ),
