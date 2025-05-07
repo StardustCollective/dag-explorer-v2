beab965d2310fcf47d459641be8baa3932007fd5
Juan David Suarez <juansuarezespitia@gmail.com>
2025-05-07 13:41:02 -0500

    Updated: tab component as generic element component (renderAs prop)

--- src/components/Tabs/Tab.tsx
+++ src/components/Tabs/Tab.tsx
@@ -5,32 +5,35 @@ import React, { useContext } from "react";
 
 import { TabsContext } from "./TabsContext";
 
-export type ITabProps = {
+export type ITabProps<T extends React.ElementType = "div"> = {
+  renderAs?: T;
   id: string;
-  href?: string;
-  children: React.ReactNode;
-};
+  children?: React.ReactNode;
+} & Omit<React.ComponentPropsWithoutRef<T>, "className" | "children">;
 
-export const Tab = ({ id, href, children }: ITabProps) => {
+export const Tab = <T extends React.ElementType = "div">({
+  renderAs,
+  id,
+  children,
+  ...props
+}: ITabProps<T>) => {
   const tabsContext = useContext(TabsContext);
 
   if (!tabsContext) {
     throw new Error("<Tabs.Tab/> must be rendered inside a <Tabs/> component");
   }
 
-  const RenderComponent = href ? "a" : "div";
+  const RenderComponent = renderAs ?? "div";
 
   return (
     <RenderComponent
-      href={href ?? ""}
       className={clsx(
         "pt-5 pb-4 text-black/65 font-semibold cursor-pointer transition-all duration-200 ease-out",
         tabsContext.value === id && "text-hgtp-blue-600"
       )}
       onClick={() => tabsContext.onValue?.(id)}
-      ref={(element: HTMLDivElement | HTMLAnchorElement | null) =>
-        tabsContext.onRef(id, element)
-      }
+      ref={(element: any) => tabsContext.onRef(id, element)}
+      {...props}
     >
       {children}
     </RenderComponent>
