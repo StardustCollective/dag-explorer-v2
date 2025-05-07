5c6583b6758fa6dc23497dec8862738e959dc0d3
Juan David Suarez <juansuarezespitia@gmail.com>
2025-05-07 13:42:38 -0500

    Added: new action transaction types

--- src/types/actions.ts
+++ src/types/actions.ts
@@ -5,12 +5,49 @@ export enum ActionTransactionType {
   SpendExpiration = "SpendExpiration",
   TokenLock = "TokenLock",
   TokenUnlock = "TokenUnlock",
+  DelegatedStake = "DelegatedStake",
+  DelegatedStakeWithdrawal = "DelegatedStakeWithdrawal",
 }
 
 export const isActionTransactionType = (
   type: any
 ): type is ActionTransactionType => {
   return Object.values(ActionTransactionType).includes(type);
+};
+
+export type IBEActionTransaction_DelegatedStake = {
+  type: ActionTransactionType.DelegatedStake;
+  hash: string;
+  ordinal: number;
+  source: string;
+  nodeId: string;
+  amount: number;
+  fee: number;
+  tokenLockRef: string;
+  parentHash: string;
+  globalSnapshotHash: string;
+  timestamp: string;
+};
+
+export type IBEActionTransaction_DelegatedStakeWithdrawal = {
+  type: ActionTransactionType.DelegatedStakeWithdrawal;
+  hash: string;
+  source: string;
+  stakeHash: string;
+  globalSnapshotHash: string;
+  timestamp: string;
+};
+
+export type IBEActionTransaction_TokenLock = {
+  type: ActionTransactionType.TokenLock;
+  hash: string;
+  amount: number;
+  source: string;
+  unlockEpoch: number | null;
+  parentHash: string;
+  ordinal: number;
+  timestamp: string;
+  unlockedAtOrdinal: number | null;
 };
 
 export type IBEActionTransaction = {
