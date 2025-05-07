c0f24c21753989a85a2aa36e22da55014d72c411
Juan David Suarez <juansuarezespitia@gmail.com>
2025-05-07 13:46:03 -0500

    Updated: seal transaction detail page

--- src/app/[network]/transactions/[transactionHash]/page.tsx
+++ src/app/[network]/transactions/[transactionHash]/page.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
 import { getNetworkFromParamsOrFail } from "@/common/network";
@@ -5,15 +6,29 @@ import { PageLayout } from "@/components/PageLayout";
 import { PageTitle } from "@/components/PageTitle";
 import { TransactionDetail } from "@/components/TransactionDetail";
 import { getTransaction } from "@/queries";
+import { shortenString } from "@/utils";
 
 
 export const revalidate = 86_400; // 24 hours - These should not change, almost immutable
 
+type ITransactionPageProps =  {
+  params: Promise<{ network: string; transactionHash: string }>;
+}
+
+export async function generateMetadata({
+  params,
+}: ITransactionPageProps): Promise<Metadata> {
+  const { transactionHash } = await params;
+
+  return {
+    title: `Txn ${shortenString(transactionHash)} - DAG Explorer`,
+    description: `Txn Details ${transactionHash} - DAG Explorer`,
+  };
+}
+
 export default async function TransactionPage({
   params,
-}: {
-  params: Promise<{ network: string; transactionHash: string }>;
-}) {
+}: ITransactionPageProps) {
   const { transactionHash } = await params;
   const network = await getNetworkFromParamsOrFail(params);
 
@@ -26,7 +41,7 @@ export default async function TransactionPage({
   return (
     <>
       <PageTitle>Transaction details</PageTitle>
-      <PageLayout className="flex flex-col gap-4 px-20 py-8" renderAs={"main"}>
+      <PageLayout className="flex flex-col gap-4 px-4 lg:px-20 py-8" renderAs={"main"}>
         <TransactionDetail network={network} transaction={transaction} />
       </PageLayout>
     </>
