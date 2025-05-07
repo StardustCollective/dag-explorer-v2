dfe2a2b7649cdb77970aa6106279bbf2d5d183d7
Juan David Suarez <juansuarezespitia@gmail.com>
2025-05-07 13:39:03 -0500

    Added: subtitle support for section

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -2,6 +2,7 @@ import clsx from "clsx";
 
 export type ISectionProps = {
   title?: React.ReactNode;
+  subtitle?: React.ReactNode;
   action?: React.ReactNode;
   className?: string | { wrapper?: string; children?: string };
   children?: React.ReactNode;
@@ -9,6 +10,7 @@ export type ISectionProps = {
 
 export const Section = ({
   title,
+  subtitle,
   action,
   className,
   children,
@@ -18,12 +20,17 @@ export const Section = ({
 
   return (
     <section className={clsx("flex flex-col gap-5", className.wrapper)}>
-      {title && (
-        <div className="flex flex-col items-center md:flex-row md:flex-nowrap md:justify-between md:items-end">
-          <h2 className="text-2xl text-hgtp-blue-900 font-medium md:text-left text-center">
-            {title}
-          </h2>
-          {action}
+      {(title || subtitle) && (
+        <div className="flex flex-col gap-3">
+          {title && (
+            <div className="flex flex-col items-center lg:flex-row lg:flex-nowrap lg:justify-between lg:items-end">
+              <h2 className="text-2xl text-hgtp-blue-900 font-medium lg:text-left text-center">
+                {title}
+              </h2>
+              {action}
+            </div>
+          )}
+          {subtitle && <p className="font-medium text-gray-600">{subtitle}</p>}
         </div>
       )}
       <div className={className.children}>{children}</div>
