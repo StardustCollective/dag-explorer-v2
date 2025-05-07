fd1e851bbf3d58261b110d4bde99fc9df2a45757
Juan David Suarez <juansuarezespitia@gmail.com>
2025-05-07 13:43:13 -0500

    Added: display custom error message on cluster upgrade event

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -5,6 +5,7 @@ import { useEffect } from "react";
 
 import { PageLayout } from "@/components/PageLayout";
 import { Section } from "@/components/Section";
+import { isClusterUpgradeError } from "@/utils/errors";
 
 export default function ClientErrorPage({
   error,
@@ -15,24 +16,44 @@ export default function ClientErrorPage({
   useEffect(() => {
     console.error("UI Error Log");
     console.error(error);
+    console.dir(error);
   }, [error]);
 
   return (
-    <PageLayout className="flex flex-col gap-10 px-20 py-8" renderAs={"main"}>
-      <Section className="flex flex-col pt-20 justify-center items-center gap-6">
-        <h1 className="text-8xl font-medium text-hgtp-blue-600">UI Error</h1>
-        <p className="text-xl font-medium max-w-180 text-center">
-          Sorry, there was an internal error while loading the app, please
-          reload the page. If the problem persists please contact our customer
-          support channels.
-        </p>
-        <pre className="text-xs font-ibm-mono font-medium max-w-180 text-center overflow-auto border border-hgtp-blue-600 bg-hgtp-blue-600/5 rounded-md">
-          {[error.digest, error.message, error.stack].join("\n")}
-        </pre>
-        <Link className="button primary outlined xl" href="/">
-          Go back to the home page
-        </Link>
-      </Section>
+    <PageLayout className="flex flex-col gap-10 px-4 lg:px-20 py-8" renderAs={"main"}>
+      {isClusterUpgradeError(error) ? (
+        <Section className="flex flex-col pt-20 justify-center items-center gap-6">
+          <h1 className="text-8xl font-medium text-hgtp-blue-600">
+            Cluster Upgrade
+          </h1>
+          <p className="text-xl font-medium max-w-180 text-center">
+            The cluster is currently undergoing maintenance and upgrades. This
+            process may take a few minutes. Please try again later. If you
+            continue to experience issues, please contact our support team.
+          </p>
+          <pre className="text-xs font-ibm-mono font-medium max-w-180 text-center overflow-auto border border-hgtp-blue-600 bg-hgtp-blue-600/5 rounded-md">
+            {[error.digest, error.message, error.stack].join("\n")}
+          </pre>
+          <Link className="button primary outlined xl" href="/">
+            Go back to the home page
+          </Link>
+        </Section>
+      ) : (
+        <Section className="flex flex-col pt-20 justify-center items-center gap-6">
+          <h1 className="text-8xl font-medium text-hgtp-blue-600">UI Error</h1>
+          <p className="text-xl font-medium max-w-180 text-center">
+            Sorry, there was an internal error while loading the app, please
+            reload the page. If the problem persists please contact our customer
+            support channels.
+          </p>
+          <pre className="text-xs font-ibm-mono font-medium max-w-180 text-center overflow-auto border border-hgtp-blue-600 bg-hgtp-blue-600/5 rounded-md">
+            {[error.digest, error.message, error.stack].join("\n")}
+          </pre>
+          <Link className="button primary outlined xl" href="/">
+            Go back to the home page
+          </Link>
+        </Section>
+      )}
     </PageLayout>
   );
 }
