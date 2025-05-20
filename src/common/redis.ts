52ca4bca912842c5d41d6df81ae0d42e2ab49f42
Juan David Suarez <juansuarezespitia@gmail.com>
2025-05-20 05:28:23 -0500

    Updated: redis connection options to disallow some connection patterns

--- src/common/redis.ts
+++ src/common/redis.ts
@@ -10,6 +10,15 @@ export const getRedis = () => {
       const redis = new Redis({
         host: process.env.REDIS_HOST,
         port: parseNumberOrDefault(process.env.REDIS_PORT, 6379),
+        connectTimeout: 10 * 1000,
+        commandTimeout: 60 * 1000,
+        maxRetriesPerRequest: 3,
+        retryStrategy: (times) => {
+          if (times > 3) {
+            return null;
+          }
+          return Math.min(times * 50, 2000);
+        },
       });
 
       await redis.ping();
